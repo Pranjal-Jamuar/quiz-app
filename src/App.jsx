2a690b4df65a5d025d49ae63998c43947ec253a9
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-03-14 02:38:22 +0530

    Routes completed API integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import "./App.css"
 import Header from "./components/Header/Header.jsx"
 import Footer from "./components/Footer/Footer.jsx"
+import Home from "./Pages/Home/Home.jsx"
+import Quiz from "./Pages/Quiz/Quiz.jsx"
+import Result from "./Pages/Result/Result.jsx"
+import { useState } from "react"
+import axios from "axios"
 function App() {
+
+  const [name, setName] = useState("")
+  const [questions, setQuestions] = useState()
+  const [score, setScore] = useState(0)
+  const fetchQuestions = async(category = "", difficulty = "") => {
+    const {data} = await axios.get(`https://opentdb.com/api.php?amount=10${
+      category && `&category=${category}`
+    }${difficulty && `&difficulty=${difficulty}`}&type=multiple`)
+    console.log(data)
+  }
   return (
-    <BrowserRouter>
+    <Router>
       <div className="app" style={{ backgroundImage: "url(./ques1.png)" }}>
         <Header />
+        <Routes>
+          <Route path="/" element={<Home name={name} setName={setName} fetchQuestions={fetchQuestions} />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
       </div>
       <Footer />
-    </BrowserRouter>
+    </Router>
   )
 }
 
---
+++ src/Pages/Quiz/Quiz.jsx
@@ -0,0 +1,5 @@
+function Quiz() {
+  return <div>Quiz Page</div>
+}
+
+export default Quiz
