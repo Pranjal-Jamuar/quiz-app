49f156fe225b6bd1fd58c65804b46df1aa689c38
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-03-22 11:51:58 +0530

    Footer link updated.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,11 @@ function Footer() {
   return (
     <div style={{ textAlign: "center", marginBottom: 10 }}>
       Made with ❤️ by{" "}
-      <a href="https://github.com/Pranjal-Jamuar" style={{ cursor: "pointer" }}>
+      <a
+        href="https://github.com/Pranjal-Jamuar"
+        target="_blank"
+        style={{ cursor: "pointer" }}
+      >
         Pranjal Jamuar
       </a>
     </div>
